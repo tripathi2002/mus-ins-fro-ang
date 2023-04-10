ce63add1b39bb01e7f1b8ce36aefa22d4d7e52fa
Vibhu Tripathi <amtri2030@gmail.com>
2023-04-10 23:28:09 +0530

    created the insight in the dashboard panel

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: [
+    './../../../node_modules/bootstrap/dist/css/bootstrap.min.css',
+    './navbar.component.css',
+  ]
 })
 export class NavbarComponent {
 
