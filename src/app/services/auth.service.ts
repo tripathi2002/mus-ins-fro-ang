c31ed9c11254e4d424d429c84ce348717fcc8e62
Vibhu Tripathi <amtri2030@gmail.com>
2023-04-12 00:43:07 +0530

    connect the login page with data base and render the user to dashboard if credantials are correct

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,25 +1,51 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 
 let API_KEY = 'http://127.0.0.1:1000/api';
 
 @Injectable()
 export class AuthService {
+  username: string = "";
+  email: string = "";
+  token: string = null;
 
   constructor(private http: HttpClient) { }
 
+  login(email: string, password: string): boolean {
+    this.http.post(`${API_KEY}/user/admin-login`, { email, password })
+      .pipe(
+        map(res => res)
+      ).subscribe((data) => {
+        console.log(data);
+        this.username = data['firstName'];
+        this.token = `Bearer ${data['token']}`;
+        this.email = data['email'];
+      });
+    if (this.token) {
+      console.log("data1");
 
-  login(user: string, password: string): boolean {
-
-    if(user === 'user' && password === 'p'){
-      localStorage.setItem('username', user);
+      localStorage.setItem('username', this.username);
+      localStorage.setItem('email', this.email);
+      localStorage.setItem('token', this.token);
+      this.username = "";
+      this.email = "";
+      this.token = null;
       return true;
+    } else {
+      return false;
     }
-    return false;
+    // if (email === this.user) {
+    //   localStorage.setItem('username', email);
+    //   return true;
+    // }
+    // return false;
   }
 
   logout(): any {
     localStorage.removeItem('username');
+    localStorage.removeItem('email');
+    localStorage.removeItem('token');
   }
 
   getUser(): any {
@@ -33,5 +59,6 @@ export class AuthService {
 }
 
 export const AUTH_PROVIDERS: Array<any> = [
-  { provide: AuthService, userClass: AuthService }
+  { provide: AuthService, useClass: AuthService },
+  { provide: API_KEY, useValue: API_KEY },
 ];
