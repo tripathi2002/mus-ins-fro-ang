954dc774102fe6d434c9e98b2650d3ae99ec2519
Vibhu Tripathi <amtri2030@gmail.com>
2023-04-28 22:22:13 +0530

    erase the junc code from product-images

--- src/app/catalog/upload/product-images/product-images.component.ts
+++ src/app/catalog/upload/product-images/product-images.component.ts
@@ -19,56 +19,23 @@ export class ProductImagesComponent implements OnInit{
   ) {
     this.title = "Upload Product Images";
     this.error = "";
-    this.id = "6437ae8cfddd621ec20a5b84";
-    // this.id = "643d8eb0213d7d659c896d82";
+    // this.id = "6437ae8cfddd621ec20a5b84";
+    this.id = "643d8eb0213d7d659c896d82";
   }
-
-  // productUpload = new FormGroup({
-  //   images: new FormControl(),
-  // });
 
   file: File[];
-  // file: File;
+  imageUrls: string[] = [];
 
   ImageUpload(event:any):void {
-    // console.log(event.target.files[0])
-    // file = event.target.files[0];
     this.file = event.target.files;
+    this.imageUrls = Array.from(this.file).map(file=> URL.createObjectURL(file));
+    console.log(this.imageUrls);
   }
-
-
-  // upload() {
-  //   // console.log(this.productUpload.value.images);
     
-  //   this.Product.upload(this.id , this.file)
-  //     .subscribe((data) => {
-  //       alert("Product Uploaded Successfully!");
-  //       console.log(data);
-  //     }, (err) => {
-  //       // console.log(err.error.message);
-  //       document.getElementById('err').style.display = 'block';
-  //       this.error = err.error.message;
-  //       setTimeout(function () {
-  //         document.getElementById('err').style.display = 'none';
-  //         this.error = "";
-  //       }.bind(this), 4000);
-  //       console.log(err);
-  //     }, () => {
-  //       // this.product['title'] = '';
-  //     });
-  // }
-
   uploads(){
     const formData = new FormData();
-    // console.log(this.productUpload.value.images.length);
-    // for (let i=0;i<this.productUpload.value.images.length;i++){
-    //   console.log(this.productUpload.value.images);
-    // }
-    console.log(this.file.length);
-    console.log(this.file);
+  
     for (let i = 0; i < this.file.length; i++) {
-      // const element = this.file[i];
-      // console.log(this.file[i]);
       formData.append('images', this.file[i]);
     }
 
@@ -82,9 +49,5 @@ export class ProductImagesComponent implements OnInit{
       )
   }
 
-
-  ngOnInit(): void {
-
-  }
-
+  ngOnInit(): void {  }
 }
